eaa9e3ab069c8b642b5315e91c5d255edd8b443a
ogamanbs <134797606+ogamanbs@users.noreply.github.com>
2024-07-28 15:33:48 +0530

    added logo on the landing page and linked it to the main site

--- app/page.js
+++ app/page.js
@@ -12,8 +12,8 @@ const Home = () => {
     <div className="relative w-full min-h-screen bg-zinc-900 text-white">
         <div className="relative flex items-center justify-center md:justify-between px-10 py-5 ">
           <div className="flex gap-3 items-center">
-            <LogoImage />
-            <h1 className="text-xl">Aman Blogs</h1>
+              <Link href="https://www.amanbs.com/"><LogoImage /></Link>
+              <h1 className="text-xl">Aman Blogs</h1>
           </div>
           <div className="text-sm hidden md:block">
             
