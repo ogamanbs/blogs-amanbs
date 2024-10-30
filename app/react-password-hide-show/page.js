66c5089d301225bf23c0667ca95e2279bf5b0db8
ogamanbs <134797606+ogamanbs@users.noreply.github.com>
2024-10-30 22:26:24 +0530

    edited the viewport height oh react-password-hide-show

--- app/react-password-hide-show/page.js
+++ app/react-password-hide-show/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const ReactPasswordShowHide = () => {
   return (
-    <div className="w-full h-full">
+    <div className="w-[100vw] h-[100vh]">
         <Password/>
     </div>
   )
